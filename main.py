5a0a9d7658637964534dd53a87b20db2fb0c64c0
MustelaErmine <50228797+MustelaErmine@users.noreply.github.com>
2022-02-11 19:51:42 +0800

    Task2

--- main.py
+++ main.py
@@ -9,24 +9,42 @@ zoom = 16
 size = (600, 450)
 
 def main():
+    global zoom
     pygame.init()
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
+
+    rerender(screen)
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == 1073741899:
+                    zoom += 1
+                    zoom = min(zoom, 17)
+                    rerender(screen)
+                elif event.key == 1073741902:
+                    zoom -= 1
+                    zoom = max(zoom, 0)
+                    rerender(screen)
+
+
+def rerender(screen):
+    pygame.draw.rect(screen, '#000000', (0, 0, size[0], size[1]))
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
     dc = {
         "ll": str(coordinates[0]) + "," + str(coordinates[1]),
         "z": str(zoom),
         "l": "map"
     }
-    map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=dc)
-    print(response.request.url)
-    print(response.status_code)
     with open('map_tmp.png', 'wb') as f:
         f.write(response.content)
     screen.blit(pygame.image.load('map_tmp.png'), (0, 0))
     pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
 
 
 if __name__ == '__main__':
